101cc5865825fa4040ef0e297d9dea6a12c10bff
LudmilKirov <ludmil9595@gmail.com>
2019-08-14 16:33:28 +0300

    add a background notification

---
+++ app/src/main/java/com/example/photogallery/PollService.java
@@ -0,0 +1,131 @@
+package com.example.photogallery;
+
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.nio.channels.AlreadyBoundException;
+import java.util.List;
+
+public class PollService extends IntentService {
+    private static final String TAG = "PollService";
+    //60 seconds
+    private static final  long POLL_INTERVAL = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+
+    //SetInexactRepeating takes four parameters: a constant to
+    // describe the time basis for the alarm,the time at which
+    // to start the alarm,the time interval at to repeat the alarm
+    // and finally a PendingIntent to fire when alarm goes off.
+    public static void setServiceAlarm(Context context, boolean isOn) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+        AlarmManager alarmManager = (AlarmManager)
+                context.getSystemService(Context.ALARM_SERVICE);
+
+        if (isOn) {
+            alarmManager.setInexactRepeating
+                    (AlarmManager.ELAPSED_REALTIME,
+                            SystemClock.elapsedRealtime(),
+                            POLL_INTERVAL, pi);
+        } else {
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PollService.class);
+    }
+
+    public PollService() {
+        super(TAG);
+    }
+
+    protected void onHandleIntent(Intent intent) {
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
+        String query = QueryPrefernces.getStoredQuery(this);
+        String lastResultId = QueryPrefernces.getLastResultId(this);
+        List<GalleryItems> items;
+
+        if (query == null) {
+            items = new FlickFetchr().fetchRecentPhotos();
+        } else {
+            items = new FlickFetchr().searchPhotos(query);
+        }
+
+        if (items.size() == 0) {
+            return;
+        }
+
+        String resultId = items.get(0).getId();
+        if (resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got an old result: " + resultId);
+        } else {
+            Log.i(TAG, "Got a new result: " + resultId);
+
+            Resources resource= getResources();
+            Intent i = MainActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this,0,i,0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(resource.getString(R.string.new_picture_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resource.getString(R.string.new_picture_title))
+                    .setContentText(resource.getString(R.string.new_picture_text))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+            NotificationManagerCompat notificationManagerCompat =
+                    NotificationManagerCompat.from(this);
+            notificationManagerCompat.notify(0,notification);
+
+        }
+
+        QueryPrefernces.setLastResultId(this, resultId);
+
+    }
+
+    //Toggling the background data setting to disallow downloading
+    // data in the background disables the network entirely for use
+    // by background services.In this case,ConnectivityManager.getActiveNetworkInfo
+    // returns null,making it appears to the background service
+    // as if there is no active network available,even if there really is.
+    //If the network is available to your background service,it gets an
+    // instance of NetworkInfo representing the current network connection.
+    // The code hen checks whether the current network is fully
+    // connected by calling NetworkInfo.isConnected
+    //If the app does not see a network available,or the device is not fully
+    // connected to a network, onHandleIntent will return
+    // without executing the rest of the method.This is a good practise
+    // because the app cannot download any data if it is not connected to the network
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        boolean isNetworkAvailable = cm.getActiveNetwork() != null;
+        boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
+
+        return isNetworkConnected;
+    }
+
+    public static boolean isServiceAlarmOn(Context context){
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent
+                .getService(context,0,i,PendingIntent.FLAG_NO_CREATE);
+
+        //Null PendingIntent means that alarm is not set
+        return pi != null;
+    }
+}
--- app/src/main/java/com/example/photogallery/QueryPrefernces.java
+++ app/src/main/java/com/example/photogallery/QueryPrefernces.java
@@ -5,6 +5,7 @@ import android.preference.PreferenceManager;
 
 public class QueryPrefernces {
     private static final String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String PREF_LAST_RESULT_ID = "lastResultId";
     //Returns the query value stored in shared preferences.
     // It does so by first acquiring the default
     // SharedPreferences for the given context.
@@ -19,6 +20,18 @@ public class QueryPrefernces {
         PreferenceManager.getDefaultSharedPreferences(context)
                 .edit()
                 .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+
+    public static String getLastResultId(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_LAST_RESULT_ID,null);
+    }
+
+    public static void setLastResultId(Context context,String lastResultId){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_LAST_RESULT_ID,lastResultId)
                 .apply();
     }
 }
