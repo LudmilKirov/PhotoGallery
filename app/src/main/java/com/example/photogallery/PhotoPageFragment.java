c0cbd36266cd9dab283866cfa512181e59f116c7
LudmilKirov <ludmil9595@gmail.com>
2019-08-15 18:11:34 +0300

    When press the photo open a WebView

---
+++ app/src/main/java/com/example/photogallery/PhotoPageFragment.java
@@ -0,0 +1,79 @@
+package com.example.photogallery;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PhotoPageFragment extends VisibleFragment {
+    private static final String ARG_URI = "photo_page_url";
+
+    private Uri mUri;
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
+
+    public static PhotoPageFragment newInstance(Uri uri) {
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_URI, uri);
+
+        PhotoPageFragment fragment = new PhotoPageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mUri = getArguments().getParcelable(ARG_URI);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
+
+        mProgressBar=
+                (ProgressBar)v.findViewById(R.id.fragment_photo_page_progress_bar);
+        //WebChromeCLient report in range 0-100
+        mProgressBar.setMax(100);
+
+        mWebView = (WebView) v.findViewById(R.id.fragment_photo_page_web_view);
+        //Turn JavaScript on by calling getSettings to get an
+        // instance of WebSettings and calling setJAvaScripEnabled.
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            public void onProgressChanged(WebView webView,int newProgress){
+                if(newProgress==100){
+                    mProgressBar.setVisibility(View.GONE);
+                }
+                else{
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+            public void onReceivedTitle(WebView webView,String title){
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
+        mWebView.setWebViewClient(new WebViewClient(){
+          public boolean shouldOverrideUrlLoading(WebView view,String url){
+              return false;
+          }
+        });
+        mWebView.loadUrl(mUri.toString());
+
+        return v;
+    }
+}
